d94199f55cfa330aef1721760d60f7bebb84e3de
User15363828181728272 <defanaldrian1329@gmail.com>
2026-03-16 12:47:55 +0700

    Upload: setting.js

---
+++ setting.js
@@ -0,0 +1,12 @@
+module.exports = {
+    ownerId:      "8412273544",
+    ownerUser:    "@defandryan",
+    botName:      "Soraa.dev",
+    channelId:    "-1003273545763",
+    channelUser:  "@depstore11",
+    botToken:     "8459562380:AAGI2LmIZJyxvSO1HKJnzkaRghxBZO7gisI",
+    netlifyToken: "nfp_AcwSmEsyJ7yp7noECVb5epgf1qsiYHbA07be",
+    apiKey:       "SRNET727",
+    domain:       "soraa.depstore.my.id",
+    startTime:    Date.now()
+};
